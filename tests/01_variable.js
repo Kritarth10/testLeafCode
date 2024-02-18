9a355be315cecfe85ef97736bc5a9dd4b7cda109
Kritarth10 <govilkritarth10@gmail.com>
2024-02-18 18:54:18 +0530

    Add files via upload

---
+++ tests/01_variable.js
@@ -0,0 +1,14 @@
+const { type } = require("os");
+
+var firstName = "Kritarth";
+var companyName = "3cLogic";
+var mobileNumber =9711908905
+var isAutomation=false;
+var hasPlaywright;
+
+console.log(typeof firstName);
+console.log(typeof companyName);
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright);
+
